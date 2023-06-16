4c45cdc4b9cc776d6ef43688a0ca02632eb6a34c
samuel-n-silva <samuel_nunes-silva@estudante.sesisenai.org.br>
2023-06-16 12:15:11 -0300

    Criado os metodos create,put,delet,updade da rota armazem

---
+++ src/main/java/com/example/squad/service/ArmazemService.java
@@ -0,0 +1,21 @@
+package com.example.squad.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.squad.model.Armazem;
+import com.example.squad.repository.ArmazemRepository;
+
+@Service
+public class ArmazemService {
+
+    private final ArmazemRepository armazemRepository;
+
+    @Autowired
+    public ArmazemService(ArmazemRepository armazemRepository) {
+        this.armazemRepository = armazemRepository;
+    }
+
+    public Armazem saveArmazem(Armazem armazem) {
+        return armazemRepository.save(armazem);
+    }
+}
