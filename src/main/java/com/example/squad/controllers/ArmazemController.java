daca2b4d07aa0ec9f7385ac8858b2627a767ebf5
samuel-n-silva <samuel_nunes-silva@estudante.sesisenai.org.br>
2023-06-24 12:19:48 -0300

    alterações na rota armazem

--- src/main/java/com/example/squad/controllers/ArmazemController.java
+++ src/main/java/com/example/squad/controllers/ArmazemController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/armazem")
 public class ArmazemController {
 
     private final ArmazemRepository armazemRepository;
@@ -18,28 +18,30 @@ public class ArmazemController {
         this.armazemRepository = armazemRepository;
     }
 
-    @GetMapping("/armazem")
+    @GetMapping
     public List<Armazem> fetchArmazemList() {
         return armazemRepository.findAll();
     }
 
-    @PostMapping("/armazem")
+    @PostMapping
     public Armazem saveArmazem(@RequestBody Armazem armazem) {
+        armazem.setSituacao("Ativo"); // Define o valor padrão "ativo" para a coluna "situacao"
         return armazemRepository.save(armazem);
     }
 
-    @PutMapping("armazem/{id}")
+    @PutMapping("/{id}")
     public Armazem updateArmazem(@PathVariable Long id, @RequestBody Armazem updatedArmazem) {
         Armazem armazem = armazemRepository.findById(id).orElse(null);
         if (armazem != null) {
             armazem.setNome(updatedArmazem.getNome());
             armazem.setAnimal(updatedArmazem.getAnimal());
+            armazem.setSituacao(updatedArmazem.getSituacao());
             return armazemRepository.save(armazem);
         }
         return null;
     }
 
-    @DeleteMapping("armazem/{id}")
+    @DeleteMapping("/{id}")
     public void deleteArmazem(@PathVariable Long id) {
         armazemRepository.deleteById(id);
     }
